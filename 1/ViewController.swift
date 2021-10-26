ae376dd1893829f2ea5cdeb59a3b4f5754399382
Toshiki Yanagimoto <toshiyana0306@gmail.com>
2021-10-26 15:02:44 +0900

    add files

--- 1/ViewController.swift
+++ 1/ViewController.swift
@@ -8,12 +8,39 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet private var numText1: UITextField!
+    @IBOutlet private var numText2: UITextField!
+    @IBOutlet private var numText3: UITextField!
+    @IBOutlet private var numText4: UITextField!
+    @IBOutlet private var numText5: UITextField!
+
+    @IBOutlet private var calcButton: UIButton!
+    @IBOutlet private var clearButton: UIButton!
+
+    @IBOutlet private var resultLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction private func calcButtonPressed() {
+        let num1 = Int(numText1.text!) ?? 0
+        let num2 = Int(numText2.text!) ?? 0
+        let num3 = Int(numText3.text!) ?? 0
+        let num4 = Int(numText4.text!) ?? 0
+        let num5 = Int(numText5.text!) ?? 0
 
+        let result = num1 + num2 + num3 + num4 + num5
+
+        resultLabel.text = String(result)
+    }
+
+    @IBAction private func clearButtonPressed() {
+        numText1.text = nil
+        numText2.text = nil
+        numText3.text = nil
+        numText4.text = nil
+        numText5.text = nil
+    }
 }
-
